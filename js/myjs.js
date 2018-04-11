5c8c0c837d665d5259caab53afbe82dee8443b37
Jakaria Istauk <jakariamd35@gmail.com>
2018-04-11 21:34:04 +0600

    Add files via upload

---
+++ js/myjs.js
@@ -0,0 +1,59 @@
+
+
+// external js: isotope.pkgd.js
+
+
+// init Isotope
+var $grid = $('.grid').isotope({
+  itemSelector: '.element-item',
+  layoutMode: 'fitRows',
+  getSortData: {
+    name: '.name',
+    symbol: '.symbol',
+    number: '.number parseInt',
+    category: '[data-category]',
+    weight: function( itemElem ) {
+      var weight = $( itemElem ).find('.weight').text();
+      return parseFloat( weight.replace( /[\(\)]/g, '') );
+    }
+  }
+});
+
+// filter functions
+var filterFns = {
+  business: function() {
+    var name = $(this).find('.name').text();
+    return name.match( "Business" );
+  },
+  corporate: function() {
+    var name = $(this).find('.name').text();
+    return name.match( "Corporate" );
+  },
+  branding: function() {
+    var name = $(this).find('.name').text();
+    return name.match( "Branding" );
+  },
+  photography: function() {
+    var name = $(this).find('.name').text();
+    return name.match( "Photography" );
+  }
+};
+
+// bind filter button click
+$('#filters').on( 'click', 'button', function() {
+  var filterValue = $( this ).attr('data-filter');
+  // use filterFn if matches value
+  filterValue = filterFns[ filterValue ] || filterValue;
+  $grid.isotope({ filter: filterValue });
+});
+
+
+// change is-checked class on buttons
+$('.button-group').each( function( i, buttonGroup ) {
+  var $buttonGroup = $( buttonGroup );
+  $buttonGroup.on( 'click', 'button', function() {
+    $buttonGroup.find('.is-checked').removeClass('is-checked');
+    $( this ).addClass('is-checked');
+  });
+});
+  
